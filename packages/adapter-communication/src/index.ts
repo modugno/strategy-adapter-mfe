fd18d1deb79131c0e9182a30b6746c1318ec6c13
Guilherme Modugno <modugnoguilherme@gmail.com>
2023-08-15 23:18:39 -0300

    chore: improves

--- packages/adapter-communication/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { detectAppShellType } from './detectAppShellType'
-export { default as getService } from './getService'
-export type { AppShellType, StrategyType } from './types'
--- packages/adapter-communication/src/types.ts
+++
@@ -1,2 +0,0 @@
-export type AppShellType = 'canal' | 'webview' | 'localhost'
-export type StrategyType<T> = Record<AppShellType, T>
